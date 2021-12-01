21b764bfd93c0b9b93a4fbdf7689121a0ee56094
Esmeralda Enriquez <78413520+kimmurielO@users.noreply.github.com>
2021-12-01 11:38:44 +0100

    Add files via upload

---
+++ src/test/java/com/nttdata/starter/ServiciostarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciostarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
