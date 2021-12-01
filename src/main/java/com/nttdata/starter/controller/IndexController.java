f7846360ae1b37ec3f72db4407506aad1d366191
Esmeralda Enriquez <78413520+kimmurielO@users.noreply.github.com>
2021-12-01 11:52:34 +0100

    Add files via upload

---
+++ src/main/java/com/nttdata/starter/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.nttdata.starter.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import starter.Saludo;
+
+@RestController
+public class IndexController {
+	
+	@Autowired
+	private Saludo saludo;
+	
+	@GetMapping("/")
+	public ResponseEntity<String> index(){
+		return new ResponseEntity<String>(HttpStatus.OK).ok(saludo.mensajeSaludo());
+	}
+
+}
